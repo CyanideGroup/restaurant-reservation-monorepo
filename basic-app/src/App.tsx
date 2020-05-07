56cbbddf3526d457797833afdc4a3e2b3654b9a1
JustynaBroniszewska <jusbroniszewska@gmail.com>
2020-05-07 22:15:57 +0200

    Add basic reservation page

--- basic-app/src/App.tsx
+++ basic-app/src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {ReservationPage} from './ReservationPage';
+import { ApiGatewayService } from './services/ApiGatewayService';
 
-const App = () => (
+const App = () => {
+  const [apiGatewayService] = useState(new ApiGatewayService('https://e4fa99cd-b170-445e-a214-791fb890585c.mock.pstmn.io'));
+  return (
   <div>
-    Hello React!
+    <ReservationPage apiGatewayService={apiGatewayService}/>
   </div>
-);
+)};
 
 export default App;
