0be576222297e8a74ab540e52fb9a1417b2fb45f
Justyna Broniszewska <33961199+JustynaBroniszewska@users.noreply.github.com>
2020-05-08 20:16:03 +0200

    Add basic styles (#11)

--- basic-app/src/App.tsx
+++ basic-app/src/App.tsx
@@ -1,11 +1,12 @@
 import React, {useState} from 'react';
 import {ReservationPage} from './ReservationPage';
 import { ApiGatewayService } from './services/ApiGatewayService';
+import './styles/main.sass';
 
 const App = () => {
   const [apiGatewayService] = useState(new ApiGatewayService('http://localhost:3030'));
   return (
-  <div>
+  <div className='app'>
     <ReservationPage apiGatewayService={apiGatewayService}/>
   </div>
 )};
