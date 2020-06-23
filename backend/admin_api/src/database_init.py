b23ea293d6d280d19198b00f9dff646f955a2210
pawel <pawelzakieta97@gmail.com>
2020-06-23 18:42:14 +0200

    database init from file

--- backend/admin_api/src/database_init.py
+++ backend/admin_api/src/database_init.py
@@ -1,7 +1,27 @@
 import datetime
+import json
+from os.path import isfile
 
 
 def get_init_data():
+
+    has_files = isfile('restaurants.json') and isfile('tables.json') and isfile('reservations.json')
+
+    if has_files:
+        restaurants_data = json.load('restaurants.json')
+        for row in restaurants_data:
+            row['opens'] = datetime.datetime.strptime(row['opens'], '%H:%M:%S').time()
+            row['closes'] = datetime.datetime.strptime(row['closes'], '%H:%M:%S').time()
+
+        tables_data = json.load('tables.json')
+        reservations_data = json.load('reservations.json')
+        for row in reservations_data:
+            row['time'] = datetime.datetime.strptime(row['time'], '%H:%M:%S').time()
+            row['date'] = datetime.datetime.strptime(row['date'], '%Y.%m.%d').date()
+        return restaurants_data, tables_data, reservations_data
+
+
+
     user_names = ['tomek', 'robert', 'justyna', 'anton', 'khanh', 'pawel']
     emails = [f'{name}@gmail.com' for name in user_names]
 
@@ -28,3 +48,10 @@ def get_init_data():
         for reserv_id, email, table_id in zip(reserv_ids, emails, table_ids)]
 
     return restaurants_data, tables_data, reservations_data
+
+if __name__ == '__main__':
+    with open('restaurants.json') as json_file:
+        data = json.load(json_file)
+        data['opens'] = datetime.datetime.strptime(data['opens'], '%H:%M:%S').time()
+        data['closes'] = datetime.datetime.strptime(data['closes'], '%H:%M:%S').time()
+        print(data)
